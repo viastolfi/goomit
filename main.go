201295d766d640a1636f1677b4a51493a16eea4f
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-10-08 23:43:43 +0200

    feat: refactor git related features

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 
 	"main/client"
 	"main/config"
@@ -12,38 +11,6 @@ import (
 
 	"golang.org/x/term"
 )
-
-func getGitDiff() (string, error) {
-	cmd := exec.Command("git", "diff", "--color=always")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", fmt.Errorf("No git repo detected in your current directory \n", err)
-	}
-
-	if len(output) == 0 {
-		return "", fmt.Errorf("No changes detected, are you sure you have something to commit ?")
-	}
-
-	return string(output), nil
-}
-
-func commit(message string) error {
-	cmd := exec.Command("git", "add", "-u")
-	_, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("Error while adding file to commit \n", err)
-	}
-
-	fmt.Println("File added!")
-	cmd = exec.Command("git", "commit", "-m", message)
-	_, err = cmd.CombinedOutput()
-	if err != nil {
-		fmt.Errorf("Error while commiting changes \n", err)
-	}
-
-	fmt.Println("Commit created!")
-	return nil
-}
 
 func main() {
 	var modelName string = "llama3"
@@ -63,7 +30,7 @@ func main() {
 		}
 	}
 
-	diff, err := getGitDiff()
+	diff, err := client.GetGitDiff()
 	if err != nil {
 		log.Fatalf("Error while getting git diff \n %s", err)
 	}
@@ -100,7 +67,7 @@ func main() {
 
 	term.Restore(int(os.Stdin.Fd()), oldState)
 	if b[0] == 'y' || b[0] == 'Y' || b[0] == '\r' {
-		if err := commit(resp); err != nil {
+		if err := client.Commit(resp); err != nil {
 			log.Fatalf("Error during commit phase : %s\nTry to commit yourself", err)
 		}
 		fmt.Println("All done ! You can now push your commits\nThanks for using goomit")
